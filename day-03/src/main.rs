b0711a703af03e783cc1b89f911eed1db8c15923
Santiago Soler <santisoler@fastmail.com>
2024-12-03 23:04:23 -0800

    Cleanup files and keep only regex solutions

--- day-03/src/main.rs
+++ day-03/src/main.rs
@@ -1,11 +1,9 @@
-mod re;
+use regex::Regex;
 use std::fs;
 
 #[cfg(test)]
 mod tests {
-    use crate::parse_line;
-    use crate::re;
-    use crate::solve_part1;
+    use super::*;
 
     #[test]
     fn test_part1() {
@@ -15,95 +13,81 @@ mod tests {
     }
 
     #[test]
-    fn test_part1_re() {
-        let fname = "data/test_input";
-        let result = re::solve_part1(fname);
-        assert_eq!(result, 161);
-    }
-
-    #[test]
     fn test_part2() {
         let fname = "data/test_input_2";
-        let result = re::solve_part2(fname);
+        let result = solve_part2(fname);
         assert_eq!(result, 48);
     }
 
     #[test]
-    fn test_parse_line() {
-        assert_eq!(parse_line("alkjmul(3,2)"), 6);
-        assert_eq!(parse_line("xmul(2,4)%"), 8);
-        assert_eq!(parse_line("xmul(2,4)%mul(5,6)"), 8 + 30);
-        assert_eq!(parse_line("do_not_mul(5,5)3"), 25);
-        assert_eq!(parse_line("xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)3"), 8 + 25);
-        assert_eq!(
-            parse_line("xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"),
-            2 * 4 + 5 * 5 + 11 * 8 + 8 * 5
-        );
+    fn test_parse_and_execute() {
+        let result = parse_and_execute("ajlfasdmul(5,4)alksjf");
+        assert_eq!(result, 20);
+    }
+
+    #[test]
+    fn test_parse_and_execute_with_do() {
+        let result =
+            parse_and_execute_with_do("ajlfasdmul(5,4)alkdon't()sjmul(3,4)lajdon't()ajdmul(3,9)f");
+        assert_eq!(result, 20);
     }
 }
 
-fn find_argument(string: &str, last_char: char) -> Option<usize> {
-    // Find numeric argument until a given last_char is encountered
-    for (i, c) in string.chars().enumerate() {
-        if c.is_numeric() {
-            continue;
+fn parse_and_execute(code: &str) -> i32 {
+    let re = Regex::new(r"mul\(([0-9]+),([0-9]+)\)").unwrap();
+    let result = {
+        let mut result = 0;
+        for capture in re.captures_iter(code) {
+            let first: i32 = (&capture[1]).parse().expect("Couldn't parse");
+            let second: i32 = (&capture[2]).parse().expect("Couldn't parse");
+            result += first * second
         }
-        if c == last_char {
-            return Some(i);
-        } else {
-            return None;
-        }
-    }
-    return None;
-}
-
-fn find_arguments(string: &str) -> Option<(usize, usize)> {
-    let comma_index: usize = match find_argument(&string, ',') {
-        Some(comma_index) => comma_index,
-        None => return None,
+        result
     };
-    let end_index: usize = match find_argument(&string[comma_index + 1..], ')') {
-        Some(end_index) => end_index + comma_index + 1,
-        None => return None,
-    };
-    return Some((comma_index, end_index));
-}
-
-fn parse_line(string: &str) -> i32 {
-    let mut string = string;
-    let mut result = 0;
-    loop {
-        match string.find("mul(") {
-            Some(index) => string = &string[index + 4..],
-            None => break,
-        };
-        let (comma, end) = match find_arguments(&string) {
-            Some(indices) => indices,
-            None => continue,
-        };
-        let first: i32 = string[..comma].parse().unwrap();
-        let second: i32 = string[comma + 1..end].parse().unwrap();
-        result += first * second;
-        string = &string[end + 1..];
-    }
     result
 }
 
-fn solve_part1(fname: &str) -> i32 {
+fn parse_and_execute_with_do(line: &str) -> i32 {
+    let re = Regex::new(r"mul\(([0-9]+),([0-9]+)\)|don't\(\)|do\(\)").unwrap();
+    let result = {
+        let mut result = 0;
+        let mut run = true;
+        for capture in re.captures_iter(line) {
+            if (&capture[0]).eq("do()") {
+                run = true;
+                continue;
+            }
+            if (&capture[0]).eq("don't()") {
+                run = false;
+                continue;
+            }
+            if run {
+                let first: i32 = (&capture[1]).parse().expect("Couldn't parse");
+                let second: i32 = (&capture[2]).parse().expect("Couldn't parse");
+                result += first * second
+            }
+        }
+        result
+    };
+    result
+}
+
+pub fn solve_part1(fname: &str) -> i32 {
     let content = fs::read_to_string(fname).expect("Couldn't read!");
-    let mut result = 0;
-    for line in content.lines() {
-        result += parse_line(&line);
-    }
-    return result;
+    let result = parse_and_execute(&content);
+    result
+}
+
+pub fn solve_part2(fname: &str) -> i32 {
+    let content = fs::read_to_string(fname).expect("Couldn't read!");
+    let result = parse_and_execute_with_do(&content);
+    result
 }
 
 fn main() {
     let fname = "data/input";
     let result = solve_part1(fname);
     println!("Solution to part 1: {result}");
-    let result = re::solve_part1(fname);
-    println!("Solution to part 1: {result}");
-    let result = re::solve_part2(fname);
+    let result = solve_part2(fname);
     println!("Solution to part 2: {result}");
 }
