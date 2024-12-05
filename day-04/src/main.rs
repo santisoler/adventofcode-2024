b469002f26bf46a45150e0360ef79968b1d5c78a
Santiago Soler <santisoler@fastmail.com>
2024-12-04 22:49:16 -0800

    Move functions related to part 1 to their own module
    Avoid using regex and just count matches in string.

--- day-04/src/main.rs
+++ day-04/src/main.rs
@@ -1,5 +1,4 @@
-use regex::Regex;
-use std::fs;
+mod first;
 
 #[cfg(test)]
 mod tests {
@@ -8,107 +7,13 @@ mod tests {
     #[test]
     fn test_part1() {
         let fname = "data/test_input";
-        let result = solve_part1(fname);
+        let result = first::solve_part1(fname);
         assert_eq!(result, 18);
     }
 }
 
-fn count_xmas(string: &str) -> i32 {
-    let re_xmas = Regex::new(r"XMAS").unwrap();
-    let re_samx = Regex::new(r"SAMX").unwrap();
-    let mut counts = re_xmas.captures_iter(string).count();
-    counts += re_samx.captures_iter(string).count();
-    counts as i32
-}
-
-fn get_columns(soup: &Vec<Vec<char>>) -> Vec<String> {
-    let m = soup[0].len();
-    let columns: Vec<String> = {
-        let mut columns = Vec::new();
-        for j in 0..m {
-            let column: String = soup.iter().map(|row| &row[j]).collect();
-            columns.push(column);
-        }
-        columns
-    };
-    return columns;
-}
-
-fn get_diagonal(soup: &Vec<Vec<char>>, index: i32) -> String {
-    let mut diagonal: Vec<char> = Vec::new();
-    let right_side = index >= 0;
-    let index = index.abs() as usize;
-    for i in 0..soup.len() - index {
-        if right_side {
-            diagonal.push(soup[i][i + index]);
-        } else {
-            diagonal.push(soup[i + index][i]);
-        };
-    }
-    diagonal.iter().collect()
-}
-
-fn get_anti_diagonal(soup: &Vec<Vec<char>>, index: i32) -> String {
-    let mut anti_diagonal: Vec<char> = Vec::new();
-    let right_side = index >= 0;
-    let index = index.abs() as usize;
-    let n = soup.len();
-    for i in 0..n - index {
-        if right_side {
-            anti_diagonal.push(soup[i + index][n - 1 - i]);
-        } else {
-            anti_diagonal.push(soup[i][n - 1 - index - i]);
-        };
-    }
-    anti_diagonal.iter().collect()
-}
-
-fn get_diagonals(soup: &Vec<Vec<char>>) -> Vec<String> {
-    let mut diagonals = Vec::new();
-    let n_diagonals: i32 = soup.len() as i32 - 1;
-    let mut i = -n_diagonals;
-    while i <= n_diagonals {
-        diagonals.push(get_diagonal(&soup, i));
-        i += 1;
-    }
-    return diagonals;
-}
-
-fn get_anti_diagonals(soup: &Vec<Vec<char>>) -> Vec<String> {
-    let mut anti_diagonals = Vec::new();
-    let n_diagonals: i32 = soup.len() as i32 - 1;
-    let mut i = -n_diagonals;
-    while i <= n_diagonals {
-        anti_diagonals.push(get_anti_diagonal(&soup, i));
-        i += 1;
-    }
-    return anti_diagonals;
-}
-
-fn solve_part1(fname: &str) -> i32 {
-    let content = fs::read_to_string(&fname).expect("Couldn't read");
-    let soup: Vec<Vec<char>> = content.lines().map(|line| line.chars().collect()).collect();
-    let mut counts = 0;
-    let rows = soup
-        .iter()
-        .map(|row| row.into_iter().collect::<String>())
-        .into_iter();
-    counts += rows.map(|s| count_xmas(&s)).sum::<i32>();
-
-    let columns = get_columns(&soup);
-    counts += columns.iter().map(|s| count_xmas(&s)).sum::<i32>();
-
-    let diagonals = get_diagonals(&soup);
-    counts += diagonals.iter().map(|s| count_xmas(&s)).sum::<i32>();
-
-    let anti_diagonals = get_anti_diagonals(&soup);
-    counts += anti_diagonals.iter().map(|s| count_xmas(&s)).sum::<i32>();
-
-    counts
-}
-
 fn main() {
     let fname = "data/input";
-    let result = solve_part1(&fname);
+    let result = first::solve_part1(&fname);
     println!("Solution to part 1: {result}");
 }
