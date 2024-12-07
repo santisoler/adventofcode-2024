03bd5e8ead0b2b31aea30eb0bf3740403ce5553e
Santiago Soler <santisoler@fastmail.com>
2024-12-07 11:12:20 -0800

    Solve day 5 part 2
    Implement a quicksort algorithm to sort the updates.

--- day-05/src/main.rs
+++ day-05/src/main.rs
@@ -1,3 +1,4 @@
+use std::cmp::Ordering;
 use std::collections::HashMap;
 use std::fs;
 
@@ -10,6 +11,32 @@ mod tests {
         let fname = "data/test_input";
         let result = solve_part1(fname);
         assert_eq!(result, 143);
+    }
+
+    #[test]
+    fn test_part2() {
+        let fname = "data/test_input";
+        let result = solve_part2(fname);
+        assert_eq!(result, 123);
+    }
+
+    #[test]
+    fn test_quicksort() {
+        let dict: HashMap<i32, Vec<i32>> = HashMap::from([(1, vec![2, 3]), (2, vec![3])]);
+        let rules = Rules { dict };
+        let sorted = vec![1, 2, 3];
+
+        let mut update = vec![1, 2, 3];
+        quicksort(&mut update[..], &rules);
+        assert_eq!(update, sorted);
+
+        let mut update = vec![3, 2, 1];
+        quicksort(&mut update[..], &rules);
+        assert_eq!(update, sorted);
+
+        let mut update = vec![2, 3, 1];
+        quicksort(&mut update[..], &rules);
+        assert_eq!(update, sorted);
     }
 }
 
@@ -48,11 +75,40 @@ impl Rules {
 fn check_update_ordered(update: &Vec<i32>, rules: &Rules) -> bool {
     for values in update.windows(2) {
         let (left, right) = (values[0], values[1]);
-        if !rules.is_lower(left, right) {
+        if let Ordering::Greater = compare(left, right, &rules) {
             return false;
         }
     }
     true
+}
+
+fn compare(left: i32, right: i32, rules: &Rules) -> Ordering {
+    match rules.is_lower(left, right) {
+        true => Ordering::Less,
+        false => Ordering::Greater,
+    }
+}
+
+fn quicksort(update: &mut [i32], rules: &Rules) {
+    if update.len() < 2 {
+        return ();
+    }
+    let pivot_index = {
+        let n = update.len();
+        let pivot = update[n - 1];
+        let mut pivot_index: usize = 0;
+        for i in 0..n - 1 {
+            if let Ordering::Less = compare(update[i], pivot, rules) {
+                update.swap(i, pivot_index);
+                pivot_index += 1;
+            }
+        }
+        update.swap(pivot_index, n - 1);
+        pivot_index
+    };
+
+    quicksort(&mut update[..pivot_index], rules);
+    quicksort(&mut update[pivot_index + 1..], rules);
 }
 
 fn solve_part1(fname: &str) -> i32 {
@@ -84,8 +140,40 @@ fn solve_part1(fname: &str) -> i32 {
     result
 }
 
+fn solve_part2(fname: &str) -> i32 {
+    let content = fs::read_to_string(fname).expect("Couldn't read file.");
+    let mut lines = content.lines();
+
+    let rules = {
+        let mut rules = Rules::new();
+        for line in lines.by_ref() {
+            if line.is_empty() {
+                break;
+            };
+            let mut values = line.split("|");
+            let left: i32 = values.next().unwrap().parse().unwrap();
+            let right: i32 = values.next().unwrap().parse().unwrap();
+            rules.add_rule(left, right);
+        }
+        rules
+    };
+
+    let mut result = 0;
+    for line in lines.by_ref() {
+        let mut update: Vec<i32> = line.split(",").map(|x| x.parse().unwrap()).collect();
+        if !check_update_ordered(&update, &rules) {
+            quicksort(&mut update[..], &rules);
+            result += update[update.len() / 2];
+        };
+    }
+
+    result
+}
+
 fn main() {
     let fname = "data/input";
     let result = solve_part1(&fname);
     println!("Solution to part 1: {result}");
+    let result = solve_part2(&fname);
+    println!("Solution to part 2: {result}");
 }
