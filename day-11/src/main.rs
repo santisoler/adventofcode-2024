6ecdae55c12d45826161f10b2b802744a22fd395
Santiago Soler <santisoler@fastmail.com>
2024-12-15 15:58:00 -0800

    Solve day 11 part 2
    Implement a simpler and way efficient solution.

--- day-11/src/main.rs
+++ day-11/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fs;
 
 #[cfg(test)]
@@ -17,21 +18,50 @@ enum Stone {
     Pair(u64, u64),
 }
 
-fn blink_once(stone: u64) -> Stone {
-    if stone == 0 {
-        return Stone::Single(1);
-    };
-    let n_digits = count_digits(stone);
-    if n_digits % 2 == 0 {
-        let left = stone / u64::pow(10, n_digits / 2);
-        let right = stone - left * u64::pow(10, n_digits / 2);
-        return Stone::Pair(left, right);
-    } else {
-        return Stone::Single(stone * 2024);
-    };
+struct Counts {
+    counts: HashMap<u64, u64>,
 }
 
-fn count_digits(integer: u64) -> u32 {
+impl Counts {
+    fn new(stones: Vec<u64>) -> Self {
+        let mut counts: HashMap<u64, u64> = HashMap::new();
+        for stone in stones {
+            counts.entry(stone).and_modify(|c| *c += 1).or_insert(1);
+        }
+        return Self { counts };
+    }
+
+    fn blink(&mut self) {
+        let mut counts: HashMap<u64, u64> = HashMap::new();
+        for (stone, quantity) in self.counts.iter() {
+            match blink_stone(*stone) {
+                Stone::Single(new_stone) => {
+                    counts
+                        .entry(new_stone)
+                        .and_modify(|c| *c += quantity)
+                        .or_insert(*quantity);
+                }
+                Stone::Pair(new_stone_1, new_stone_2) => {
+                    counts
+                        .entry(new_stone_1)
+                        .and_modify(|c| *c += quantity)
+                        .or_insert(*quantity);
+                    counts
+                        .entry(new_stone_2)
+                        .and_modify(|c| *c += quantity)
+                        .or_insert(*quantity);
+                }
+            }
+        }
+        self.counts = counts;
+    }
+
+    fn count_stones(&self) -> u64 {
+        self.counts.values().sum()
+    }
+}
+
+fn count_digits(integer: u64) -> u64 {
     if integer == 0 {
         return 1;
     };
@@ -47,19 +77,19 @@ fn count_digits(integer: u64) -> u32 {
     return n_digits;
 }
 
-fn count_stones(stone: u64, n_blinks: u64) -> u64 {
-    // Count how many stones are generated after `n_blinks`.
-    if n_blinks == 0 {
-        return 1;
+fn blink_stone(stone: u64) -> Stone {
+    // Blink a single stone
+    if stone == 0 {
+        return Stone::Single(1);
     };
-    let result = match blink_once(stone) {
-        Stone::Single(s) => count_stones(s, n_blinks - 1),
-        Stone::Pair(s1, s2) => [s1, s2]
-            .iter()
-            .map(|s| count_stones(*s, n_blinks - 1))
-            .sum(),
+    let n_digits = count_digits(stone);
+    if n_digits % 2 == 0 {
+        let left = stone / u64::pow(10, n_digits as u32 / 2);
+        let right = stone - left * u64::pow(10, n_digits as u32 / 2);
+        return Stone::Pair(left, right);
+    } else {
+        return Stone::Single(stone * 2024);
     };
-    result
 }
 
 fn read_file(fname: &str) -> Vec<u64> {
@@ -72,13 +102,29 @@ fn read_file(fname: &str) -> Vec<u64> {
 }
 
 fn solve_part_one(fname: &str) -> u64 {
+    let n_blinks = 25;
     let stones = read_file(fname);
-    let n_stones = stones.iter().map(|s| count_stones(*s, 25)).sum();
-    n_stones
+    let mut counts = Counts::new(stones);
+    for _ in 0..n_blinks {
+        counts.blink()
+    }
+    counts.count_stones()
+}
+
+fn solve_part_two(fname: &str) -> u64 {
+    let n_blinks = 75;
+    let stones = read_file(fname);
+    let mut counts = Counts::new(stones);
+    for _ in 0..n_blinks {
+        counts.blink()
+    }
+    counts.count_stones()
 }
 
 fn main() {
     let fname = "data/input";
     let result = solve_part_one(fname);
     println!("Solution to part one: {result}");
+    let result = solve_part_two(fname);
+    println!("Solution to part two: {result}");
 }
