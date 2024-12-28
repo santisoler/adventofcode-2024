917e9796d7058b892afd5b2032665d4eacd23730
Santiago Soler <santisoler@fastmail.com>
2024-12-27 16:48:03 -0800

    Keep drafting a solution
    This implementation passes the tests on the simple inputs, but it fails
    to provide the right result on the personal input.

--- day-15/src/main.rs
+++ day-15/src/main.rs
@@ -17,6 +17,13 @@ mod tests {
         let result = solve_part_one(fname);
         assert_eq!(result, 10092);
     }
+
+    #[test]
+    fn test_part_two() {
+        let fname = "data/test_input";
+        let result = solve_part_two(fname);
+        assert_eq!(result, 9021);
+    }
 }
 
 fn read_file(fname: &str) -> (Map, Robot, Vec<Direction>) {
@@ -52,7 +59,55 @@ fn read_file(fname: &str) -> (Map, Robot, Vec<Direction>) {
             )
         }
     }
-    (Map { map }, robot, directions)
+    (Map::new_from(map), robot, directions)
+}
+
+fn read_file_large_map(fname: &str) -> (Map, Robot, Vec<Direction>) {
+    let content = fs::read_to_string(fname).unwrap();
+    let mut read_map = true;
+    let mut map: Vec<Vec<char>> = vec![];
+    let mut directions: Vec<Direction> = vec![];
+    let mut robot: Robot = Robot::new();
+    for (j, line) in content.lines().enumerate() {
+        if line.is_empty() {
+            read_map = false;
+            continue;
+        }
+        if read_map {
+            let mut row: Vec<char> = vec![];
+            for (i, char) in line.chars().enumerate() {
+                match char {
+                    '#' => {
+                        row.push('#');
+                        row.push('#')
+                    }
+                    'O' => {
+                        row.push('[');
+                        row.push(']')
+                    }
+                    '.' => {
+                        row.push('.');
+                        row.push('.')
+                    }
+                    '@' => {
+                        row.push('.');
+                        row.push('.');
+                        robot.x = 2 * i as i32;
+                        robot.y = j as i32;
+                    }
+                    e => panic!("invalid character {}", e),
+                }
+            }
+            map.push(row)
+        } else {
+            directions.extend(
+                line.chars()
+                    .map(|c| Direction::from(&c))
+                    .collect::<Vec<Direction>>(),
+            )
+        }
+    }
+    (Map::new_from(map), robot, directions)
 }
 
 fn solve_part_one(fname: &str) -> i32 {
@@ -64,11 +119,31 @@ fn solve_part_one(fname: &str) -> i32 {
     }
     println!("\nEnd state");
     map.print(&robot);
-    map.get_gps()
+    map.get_gps(false)
+}
+
+fn solve_part_two(fname: &str) -> i32 {
+    let (mut map, mut robot, directions) = read_file_large_map(fname);
+    println!("Start state");
+    map.print(&robot);
+    for direction in directions.iter() {
+        map.move_robot_large(&mut robot, direction);
+    }
+    println!("\nEnd state");
+    map.print(&robot);
+    map.get_gps(true)
 }
 
 fn main() {
     let fname = "data/input";
+    println!("------------");
+    println!("| Part one |");
+    println!("------------");
     let result = solve_part_one(fname);
     println!("Solution to part one: {result}");
+    println!("\n------------");
+    println!("| Part two |");
+    println!("------------");
+    let result = solve_part_two(fname);
+    println!("Solution to part two: {result}");
 }
--- day-15/src/map.rs
+++ day-15/src/map.rs
@@ -1,24 +1,32 @@
-use crate::directions::Direction;
-use crate::robot::Robot;
+use crate::Direction;
+use crate::Robot;
 
 pub struct Map {
     pub map: Vec<Vec<char>>,
+    nrows: i32,
+    ncols: i32,
 }
 
 impl Map {
-    fn nrows(&self) -> i32 {
-        self.map[0].len() as i32
-    }
-
-    fn ncols(&self) -> i32 {
-        self.map.len() as i32
+    // Create new MapLarge
+    pub fn new_from(map: Vec<Vec<char>>) -> Self {
+        let nrows = map[0].len() as i32;
+        let ncols = map.len() as i32;
+        Self { map, nrows, ncols }
     }
 
     fn get(&self, x: i32, y: i32) -> Option<char> {
-        if (x < 0) | (x >= self.nrows()) | (y < 0) | (y >= self.ncols()) {
+        if (x < 0) | (x >= self.nrows) | (y < 0) | (y >= self.ncols) {
             return None;
         }
         Some(self.map[y as usize][x as usize])
+    }
+
+    fn write(&mut self, x: i32, y: i32, value: char) {
+        if (x < 0) | (x >= self.nrows) | (y < 0) | (y >= self.ncols) {
+            panic!("trying to write outside the map")
+        }
+        self.map[y as usize][x as usize] = value;
     }
 
     pub fn move_robot(&mut self, robot: &mut Robot, direction: &Direction) {
@@ -31,6 +39,26 @@ impl Map {
             Some('#') => (), // cannot move into a wall
             Some('O') => {
                 if self.move_box(direction, x_next, y_next) {
+                    robot.x = x_next;
+                    robot.y = y_next
+                }
+            }
+            Some(e) => panic!("Invalid char {} in {}, {}", e, robot.x, robot.y),
+            None => panic!("Invalid location: {}, {}", robot.x, robot.y),
+        }
+    }
+
+    // Move robot in map with large boxes
+    pub fn move_robot_large(&mut self, robot: &mut Robot, direction: &Direction) {
+        let (x_next, y_next) = direction.get_next_position(robot.x, robot.y);
+        match self.get(x_next, y_next) {
+            Some('.') => {
+                robot.x = x_next;
+                robot.y = y_next
+            }
+            Some('#') => (), // cannot move into a wall
+            Some('[') | Some(']') => {
+                if self.move_large_box(direction, x_next, y_next) {
                     robot.x = x_next;
                     robot.y = y_next
                 }
@@ -71,6 +99,118 @@ impl Map {
         }
     }
 
+    // Try to move a large box to a given direction.
+    // If the box was moved, return true. Else return false.
+    pub fn move_large_box(&mut self, direction: &Direction, x: i32, y: i32) -> bool {
+        let box_side = match self.get(x, y) {
+            Some(c) => c,
+            None => panic!("Invalid location: {}, {}", x, y),
+        };
+        match (box_side, direction) {
+            ('[', Direction::Right) | (']', Direction::Left) => {
+                let (x_next, y_next) = direction.get_next_position(x, y);
+                let moved = self.move_large_box(direction, x_next, y_next);
+                if moved {
+                    self.write(x, y, '.');
+                    self.write(x_next, y_next, box_side);
+                }
+                return moved;
+            }
+            (']', Direction::Right) | ('[', Direction::Left) => {
+                let (x_next, y_next) = direction.get_next_position(x, y);
+                match self.get(x_next, y_next) {
+                    Some('#') => return false,
+                    Some('[') | Some(']') => {
+                        let moved = self.move_large_box(direction, x_next, y_next);
+                        if moved {
+                            self.write(x_next, y_next, box_side);
+                        }
+                        return moved;
+                    }
+                    Some('.') => {
+                        self.write(x_next, y_next, box_side);
+                        return true;
+                    }
+                    Some(e) => panic!("invalid char: {}", e),
+                    None => panic!("invalid location: {}, {}", x_next, y_next),
+                }
+            }
+            (']', Direction::Up) | (']', Direction::Down) => {
+                // let the next branch decide the vertical movement of the box
+                return self.move_large_box(direction, x - 1, y);
+            }
+            ('[', Direction::Up) | ('[', Direction::Down) => {
+                let (x_left, y_left) = direction.get_next_position(x, y);
+                let (x_right, y_right) = direction.get_next_position(x + 1, y);
+                let left = match self.get(x_left, y_left) {
+                    Some(c) => c,
+                    None => panic!("invalid location: {}, {}", x_left, y_left),
+                };
+                let right = match self.get(x_right, y_right) {
+                    Some(c) => c,
+                    None => panic!("invalid location: {}, {}", x_right, y_right),
+                };
+                match (left, right) {
+                    ('#', _) | (_, '#') => return false,
+                    ('[', ']') => {
+                        let moved = self.move_large_box(direction, x_left, y_left);
+                        if moved {
+                            self.write(x, y, '.');
+                            self.write(x + 1, y, '.');
+                            self.write(x_left, y_left, '[');
+                            self.write(x_right, y_right, ']');
+                        }
+                        return moved;
+                    }
+                    (']', '[') => {
+                        let moved_left = self.move_large_box(direction, x_left, y_left);
+                        let moved_right = self.move_large_box(direction, x_right, y_right);
+                        let moved = moved_left && moved_right;
+                        if moved {
+                            self.write(x, y, '.');
+                            self.write(x + 1, y, '.');
+                            self.write(x_left, y_left, '[');
+                            self.write(x_right, y_right, ']');
+                        }
+                        return moved;
+                    }
+                    (']', '.') => {
+                        let moved_left = self.move_large_box(direction, x_left, y_left);
+                        if moved_left {
+                            self.write(x, y, '.');
+                            self.write(x + 1, y, '.');
+                            self.write(x_left, y_left, '[');
+                            self.write(x_right, y_right, ']');
+                        }
+                        return moved_left;
+                    }
+                    ('.', '[') => {
+                        let moved_right = self.move_large_box(direction, x_right, y_right);
+                        if moved_right {
+                            self.write(x, y, '.');
+                            self.write(x + 1, y, '.');
+                            self.write(x_left, y_left, '[');
+                            self.write(x_right, y_right, ']');
+                        }
+                        return moved_right;
+                    }
+                    ('.', '.') => {
+                        self.write(x, y, '.');
+                        self.write(x + 1, y, '.');
+                        self.write(x_left, y_left, '[');
+                        self.write(x_right, y_right, ']');
+                        return true;
+                    }
+                    (l, r) => panic!("invalid chars: {}, {}", l, r),
+                }
+            }
+            (_, _) => panic!(
+                "Tried to move a tile that doesn't have a box: {} {} {}",
+                x, y, box_side
+            ),
+        }
+    }
+
     pub fn print(&self, robot: &Robot) {
         let mut map = self.map.clone();
         map[robot.y as usize][robot.x as usize] = '@';
@@ -88,11 +228,15 @@ impl Map {
     }
 
     // Return the sum of the GPS coordinates of every box in the map
-    pub fn get_gps(&self) -> i32 {
+    pub fn get_gps(&self, large: bool) -> i32 {
+        let target = match large {
+            true => '[',
+            false => 'O',
+        };
         let mut gps = 0;
         for (j, row) in self.map.iter().enumerate() {
             for (i, char) in row.iter().enumerate() {
-                if *char == 'O' {
+                if *char == target {
                     gps += (i + 100 * j) as i32;
                 }
             }
