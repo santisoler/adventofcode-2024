a8ed817b4cded8f2fd49509db176f10021e469e7
Santiago Soler <santisoler@fastmail.com>
2025-01-01 13:50:41 -0800

    Solve day 17 part 2

--- day-17/src/main.rs
+++ day-17/src/main.rs
@@ -57,19 +57,26 @@ mod tests {
         let result = solve_part_one(fname);
         assert_eq!(result, vec![4, 6, 3, 5, 6, 3, 5, 2, 1, 0]);
     }
+
+    #[test]
+    fn test_part_two() {
+        let fname = "data/test_input_2";
+        let result = solve_part_two(fname);
+        assert_eq!(result, 117440);
+    }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct ThreeBitsComputer {
-    a: u32,
-    b: u32,
-    c: u32,
+    a: u64,
+    b: u64,
+    c: u64,
     program: Vec<u8>,
     pointer: usize,
 }
 
 impl ThreeBitsComputer {
-    fn initialize(a: u32, b: u32, c: u32, program: Vec<u8>) -> Self {
+    fn initialize(a: u64, b: u64, c: u64, program: Vec<u8>) -> Self {
         Self {
             a,
             b,
@@ -79,22 +86,50 @@ impl ThreeBitsComputer {
         }
     }
 
+    /// Run a full program
     fn run(&mut self) -> Vec<u8> {
         let mut output: Vec<u8> = vec![];
         while self.pointer < self.program.len() {
-            let instruction = self.program[self.pointer];
-            let operand = self.program[self.pointer + 1];
-            self.pointer += 2;
-            match instruction {
-                0 => self.adv(operand),
-                1 => self.bxl(operand),
-                2 => self.bst(operand),
-                3 => self.jnz(operand),
-                4 => self.bxc(operand),
-                5 => output.push(self.out(operand)),
-                6 => self.bdv(operand),
-                7 => self.cdv(operand),
-                e => panic!("invalid instruction {e}"),
+            match self.run_cycle() {
+                Some(o) => output.push(o),
+                None => (),
+            };
+        }
+        output
+    }
+
+    /// Run one CPU cycle (execute a single instruction)
+    fn run_cycle(&mut self) -> Option<u8> {
+        let instruction = self.program[self.pointer];
+        let operand = self.program[self.pointer + 1];
+        self.pointer += 2;
+        match instruction {
+            0 => self.adv(operand),
+            1 => self.bxl(operand),
+            2 => self.bst(operand),
+            3 => self.jnz(operand),
+            4 => self.bxc(operand),
+            5 => return Some(self.out(operand)),
+            6 => self.bdv(operand),
+            7 => self.cdv(operand),
+            e => panic!("invalid instruction {e}"),
+        }
+        None
+    }
+
+    /// Run full iteration of the program
+    fn run_iteration(&mut self) -> u8 {
+        // Force pointer to be at zero at the start of the iteration.
+        self.pointer = 0;
+        let mut output = 0;
+        loop {
+            match self.run_cycle() {
+                Some(o) => output = o,
+                None => (),
+            }
+            // Finish iteration when the pointer jumpted to zero or when it's outside the program.
+            if self.pointer == 0 || self.pointer >= self.program.len() - 1 {
+                break;
             }
         }
         output
@@ -103,33 +138,33 @@ impl ThreeBitsComputer {
     fn get_combo_operand(&self, operand: u8) -> u32 {
         match operand {
             0..=3 => operand as u32,
-            4 => self.a,
-            5 => self.b,
-            6 => self.c,
+            4 => self.a as u32,
+            5 => self.b as u32,
+            6 => self.c as u32,
             7 => panic!("Found invalid combo operand 7"),
             e => panic!("Found invalid combo operand '{e}'"),
         }
     }
 
-    // Division between A and 2.pow(combo operand), truncate result and store in A.
+    /// Division between A and 2.pow(combo operand), truncate result and store in A.
     fn adv(&mut self, operand: u8) {
         let combo_operand = self.get_combo_operand(operand);
-        self.a = self.a / 2_u32.pow(combo_operand);
+        self.a = self.a / 2_u64.pow(combo_operand);
     }
 
-    // Bitwise XOR between B and literal operand. Result stored in B.
+    /// Bitwise XOR between B and literal operand. Result stored in B.
     fn bxl(&mut self, operand: u8) {
-        self.b = self.b ^ operand as u32;
+        self.b = self.b ^ operand as u64;
     }
 
-    // Modulo between combo operand and 8. Results stored in B.
+    /// Modulo between combo operand and 8. Results stored in B.
     fn bst(&mut self, operand: u8) {
         let combo_operand = self.get_combo_operand(operand);
-        self.b = combo_operand % 8;
+        self.b = (combo_operand % 8) as u64;
     }
 
-    // Nothing if a = 0. Else, jump pointer to the position given by the literal operand.
-    // In such case, the pointer doesn't jumps two steps after running this instruction.
+    /// Nothing if a = 0. Else, jump pointer to the position given by the literal operand.
+    /// In such case, the pointer doesn't jumps two steps after running this instruction.
     fn jnz(&mut self, operand: u8) {
         if self.a == 0 {
             return ();
@@ -137,27 +172,27 @@ impl ThreeBitsComputer {
         self.pointer = operand as usize;
     }
 
-    // Bitwise XOR between B and C. Result stored in B.
+    /// Bitwise XOR between B and C. Result stored in B.
     fn bxc(&mut self, _operand: u8) {
         self.b = self.b ^ self.c;
     }
 
-    // Combo operand % 8 and then output the value
+    /// Combo operand % 8 and then output the value
     fn out(&mut self, operand: u8) -> u8 {
         let combo_operand = self.get_combo_operand(operand);
         (combo_operand % 8) as u8
     }
 
-    // Division between A and 2.pow(combo operand), truncate result and store in B.
+    /// Division between A and 2.pow(combo operand), truncate result and store in B.
     fn bdv(&mut self, operand: u8) {
         let combo_operand = self.get_combo_operand(operand);
-        self.b = self.a / 2_u32.pow(combo_operand);
+        self.b = self.a / 2_u64.pow(combo_operand);
     }
 
-    // Division between A and 2.pow(combo operand), truncate result and store in C.
+    /// Division between A and 2.pow(combo operand), truncate result and store in C.
     fn cdv(&mut self, operand: u8) {
         let combo_operand = self.get_combo_operand(operand);
-        self.c = self.a / 2_u32.pow(combo_operand);
+        self.c = self.a / 2_u64.pow(combo_operand);
     }
 }
 
@@ -178,7 +213,7 @@ fn read_file(fname: &str) -> ThreeBitsComputer {
             .last()
             .unwrap()
             .to_lowercase();
-        let value: u32 = parts.next().unwrap().trim().parse().unwrap();
+        let value: u64 = parts.next().unwrap().trim().parse().unwrap();
         match &reg[..] {
             "a" => registers[0] = value,
             "b" => registers[1] = value,
@@ -203,14 +238,84 @@ fn fmt_output(output: &Vec<u8>) -> String {
     output.join(",")
 }
 
+/// Find the value of A that would make the computer to produce the same program.
+///
+/// This is a recursive function. The `a` argument should be the possible solution of A from the
+/// previous step (or zero if it's the first step).
+/// The iteration argument should be the index of the iteration that will be tested (use
+/// `program.len() - 1` if  this is the first step).
+fn solve_for_a(computer: &mut ThreeBitsComputer, a: u64, iteration: i32) -> Option<u64> {
+    if iteration < 0 {
+        return Some(a);
+    }
+    let expected = computer.program[iteration as usize];
+    for next_bits in 0..=0b111 {
+        let a_try = a * 2u64.pow(3 as u32) + next_bits;
+        computer.a = a_try;
+        let output = computer.run_iteration();
+        if output == expected {
+            if let Some(s) = solve_for_a(computer, a_try, iteration - 1) {
+                return Some(s);
+            }
+        }
+    }
+    None
+}
+
 fn solve_part_one(fname: &str) -> Vec<u8> {
     let mut computer = read_file(fname);
-    println!("{:?}", computer);
     computer.run()
+}
+
+/// Solve part two of the puzzle.
+///
+/// Apply a DFS recursive algorithm to construct A three-bits at a time.
+///
+/// This solution works under the following assumptions:
+///
+/// * An iteration is a single run of all instructions in the program code sequentially.
+///   We assume the program doesn't jump the pointer (besides the regular 2 steps) until the very
+///   last instruction.
+/// * The last instruction is `3,0`: jump pointer to the begining if A is not zero, halt otherwise.
+/// * Each iteration overwrites the values of registers B and C, so no state of B and C should be
+///   kept between iterations.
+/// * Each iteration "pops" the last three bits of A, and that's the only change applied to the
+///   register A. This means that it only runs adv (optcode 0) only once per iteration and always
+///   with an operator 3 (`0,3`).
+/// * Each iteration prints out a single time.
+///
+/// Both my personal input and the example provided by AoC satisfy these conditions.
+///
+/// With these assumptions in mind we can construct the bits of A by sets of three bits. We know
+/// that A will have 3N bits, where N is the number of iterations (i.e. number of values in the
+/// program code if A is a vaid solution). Since the last three bits of A are popped on each
+/// iteration, the iteration n doesn't need to know about the last 3n bits of A. This means that
+/// the last iteration (the ones that should print out `3` and leave A equal to zero) will only
+/// care about the first three bits of A (since all the other ones were already popped by the
+/// previous iterations). Therefore, we can start building A from its first three-bits, trying to
+/// find which ones can output the same number as the corresponding value in the program.
+/// By iterating over the numbers in the program code in reverse order (start from the last one
+/// -`3`-, then with the previous one -`0`- and so on), we can build a value of A that can produce
+/// the same code as the program.
+///
+/// There could be multiple solutions of three-bits for each one of the steps. But that doesn't
+/// mean they are all valid. So, we need to be able to discard solutions. Moreover, we need to find
+/// the lowest possible solution for A.
+/// To solve it, I implemented a DFS algorithm implemented in the `solve_for_a` recursive function.
+fn solve_part_two(fname: &str) -> u64 {
+    let mut computer = read_file(fname);
+    let n_iterations = computer.program.len() - 1;
+    // Start solve_for_a with a starting `a = 0` and `iteration` as the last one.
+    match solve_for_a(&mut computer, 0, n_iterations as i32) {
+        Some(a) => a,
+        None => panic!("Couldn't find optimal value for a"),
+    }
 }
 
 fn main() {
     let fname = "data/input";
     let result = solve_part_one(fname);
     println!("Solution to part one: {}", fmt_output(&result));
+    let result = solve_part_two(fname);
+    println!("Solution to part two: {result}");
 }
