76415a3b4e87b8afd72ceb62f6d2804e6bb18a82
Santiago Soler <santisoler@fastmail.com>
2025-01-19 17:19:07 -0800

    Start drafting ideas to solve day 24 part 2

--- day-24/src/main.rs
+++ day-24/src/main.rs
@@ -20,6 +20,12 @@ mod tests {
         let result = solve_part_one(fname);
         assert_eq!(result, 2024);
     }
+
+    #[test]
+    fn test_int_to_bools() {
+        let result = int_to_bools(0b1100110);
+        assert_eq!(result, vec![false, true, true, false, false, true, true]);
+    }
 }
 
 type Rules = HashMap<String, (LogicGate, String, String)>;
@@ -70,7 +76,7 @@ fn parse_bool(string: &str) -> bool {
     }
 }
 
-fn bools_to_int(bools: Vec<bool>) -> u64 {
+fn bools_to_int(bools: &Vec<bool>) -> u64 {
     let mut result = 0;
     for (i, bool) in bools.iter().enumerate() {
         if *bool {
@@ -78,6 +84,22 @@ fn bools_to_int(bools: Vec<bool>) -> u64 {
         }
     }
     result
+}
+
+fn int_to_bools(integer: u64) -> Vec<bool> {
+    let mut bools = vec![];
+    let mut tmp = integer;
+    while tmp != 0 {
+        // Get last significant bit with an AND operator
+        let bit = match tmp & 1 {
+            1 => true,
+            0 => false,
+            _ => panic!(),
+        };
+        tmp >>= 1; // shift the integer one bit to the left (remove the last significant one)
+        bools.push(bit);
+    }
+    bools
 }
 
 fn read_file(fname: &str) -> (Rules, Stack) {
@@ -131,7 +153,6 @@ fn solve_part_one(fname: &str) -> u64 {
             .map(|k| k.to_string())
             .collect();
         zetas.sort();
-        // zetas.reverse();
         zetas
     };
     let mut zetas_values: Vec<bool> = vec![];
@@ -139,7 +160,43 @@ fn solve_part_one(fname: &str) -> u64 {
         let value = get_value(&zeta, &rules, &stack);
         zetas_values.push(value);
     }
-    bools_to_int(zetas_values)
+    bools_to_int(&zetas_values)
+}
+
+fn _get_var(stack: &Stack, starts_with: &str) -> u64 {
+    let vars = {
+        let mut vars_names: Vec<String> = stack
+            .keys()
+            .filter(|k| k.starts_with(starts_with))
+            .map(|k| k.to_string())
+            .collect();
+        vars_names.sort();
+        let x: Vec<bool> = vars_names.iter().map(|x| *stack.get(x).unwrap()).collect();
+        x
+    };
+    bools_to_int(&vars)
+}
+
+fn get_x_and_y(stack: &Stack) -> (u64, u64) {
+    (_get_var(stack, "x"), _get_var(stack, "y"))
+}
+
+fn solve_part_two(fname: &str) {
+    let (rules, stack) = read_file(fname);
+    let (x, y) = get_x_and_y(&stack);
+    let expected_z = int_to_bools(x + y);
+    let (x, y) = (int_to_bools(x), int_to_bools(y));
+    for i in 0..x.len() {
+        let z_variable = format!("z{:02}", i);
+        let z = get_value(&z_variable, &rules, &stack);
+        println!(
+            "{i}: xi: {}, yi: {}, zi: {}, expected_zi: {}",
+            x[i], y[i], z, expected_z[i]
+        );
+        if z != expected_z[i] {
+            println!("{:?}", rules.get(&z_variable));
+        }
+    }
 }
 
 fn main() {
@@ -149,4 +206,7 @@ fn main() {
     let end = Instant::now();
     println!("Solution to part one: {result}");
     println!("Elapsed time: {}s", (end - start).as_secs_f64());
+
+    // let fname = "data/test_input_02";
+    let result = solve_part_two(fname);
 }
